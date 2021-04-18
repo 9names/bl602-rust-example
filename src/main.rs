482e5a5f640c8420859ff113ac36c89513d3d55b
9names <60134748+9names@users.noreply.github.com>
2021-04-18 23:11:29 +1000

    Simplify string printing example

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use bl602_hal as hal;
+use core::fmt::Write;
 use hal::{
     clock::{Strict, SysclkFreq, UART_PLL_FREQ},
     pac,
@@ -14,12 +15,14 @@ use panic_halt as _;
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
     let mut parts = dp.GLB.split();
+
     // Set up all the clocks we need
     let clocks = Strict::new()
         .use_pll(40_000_000u32.Hz())
         .sys_clk(SysclkFreq::Pll160Mhz)
         .uart_clk(UART_PLL_FREQ.Hz())
         .freeze(&mut parts.clk_cfg);
+
     // Set up uart output. Since this microcontroller has a pin matrix,
     // we need to set up both the pins and the muxs
     let pin16 = parts.pin16.into_uart_sig0();
@@ -37,40 +40,17 @@ fn main() -> ! {
     // Also set up a pin as GPIO, to blink an LED
     let mut gpio5 = parts.pin5.into_pull_down_output();
 
-    // Print some characters to let you know we're running!
-    serial.try_write(b'R').ok();
-    nb::block!(serial.try_flush()).ok();
-    serial.try_write(b'u').ok();
-    nb::block!(serial.try_flush()).ok();
-    serial.try_write(b's').ok();
-    nb::block!(serial.try_flush()).ok();
-    serial.try_write(b't').ok();
-    nb::block!(serial.try_flush()).ok();
-    serial.try_write(b'\r').ok();
-    nb::block!(serial.try_flush()).ok();
-    serial.try_write(b'\n').ok();
-    nb::block!(serial.try_flush()).ok();
-
     // Create a blocking delay function based on the current cpu frequency
     let mut d = bl602_hal::delay::McycleDelay::new(clocks.sysclk().0);
 
     loop {
-        // Okay, now lets have some fun.
-        let led_on_str = "LEDs on\r\n";
+        // Toggle the LED on and off once a second. Report LED status over UART
         gpio5.try_set_high().unwrap();
-        for c in led_on_str.bytes() {
-            serial.try_write(c).ok();
-            nb::block!(serial.try_flush()).ok();
-        }
+        serial.write_str("LEDs on\r\n").ok();
         d.try_delay_ms(1000).unwrap();
 
-        let led_off_str = "LEDs off\r\n";
         gpio5.try_set_low().unwrap();
-
-        for c in led_off_str.bytes() {
-            serial.try_write(c).ok();
-            nb::block!(serial.try_flush()).ok();
-        }
+        serial.write_str("LEDs off\r\n").ok();
         d.try_delay_ms(1000).unwrap();
     }
 }
