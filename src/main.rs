f6c068ab0c3aeff4590acb621052fe5645d0c913
9names <60134748+9names@users.noreply.github.com>
2021-05-05 20:52:02 +1000

    Adding example testing multiple rapid i2c transactions

--- src/main.rs
+++ src/main.rs
@@ -11,46 +11,76 @@ use hal::{
 };
 use panic_halt as _;
 
+const I2C_CMDS: [[u8; 2]; 8] = [
+    [0x0, 0x0],
+    [0x1, 0x0],
+    [0x04, 0x0],
+    [0x05, 0x0],
+    [0x06, 0x0],
+    [0x07, 0x0],
+    [0x14, 0xff],
+    [0x15, 0xff],
+];
+
 #[riscv_rt::entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
     let mut parts = dp.GLB.split();
 
-    // Set up all the clocks we need
     let clocks = Strict::new()
         .use_pll(40_000_000u32.Hz())
         .sys_clk(SysclkFreq::Pll160Mhz)
         .uart_clk(UART_PLL_FREQ.Hz())
+        .i2c_clk(10_000_000u32.Hz())
         .freeze(&mut parts.clk_cfg);
 
-    // Set up uart output. Since this microcontroller has a pin matrix,
-    // we need to set up both the pins and the muxs
     let pin16 = parts.pin16.into_uart_sig0();
     let pin7 = parts.pin7.into_uart_sig7();
     let mux0 = parts.uart_mux0.into_uart0_tx();
     let mux7 = parts.uart_mux7.into_uart0_rx();
 
-    // Configure our UART to 2MBaud, and use the pins we configured above
     let mut serial = Serial::uart0(
         dp.UART,
         Config::default().baudrate(2_000_000.Bd()),
         ((pin16, mux0), (pin7, mux7)),
         clocks,
     );
-    // Also set up a pin as GPIO, to blink an LED
-    let mut gpio5 = parts.pin5.into_pull_down_output();
+    let mut gpio5 = parts.pin5.into_floating_output();
+
+    let sda = parts.pin1.into_i2c_sda();
+    let scl = parts.pin2.into_i2c_scl();
+    let mut i2c = hal::i2c::I2c::i2c(dp.I2C, (scl, sda), 200_000u32.Hz(), clocks);
 
     // Create a blocking delay function based on the current cpu frequency
     let mut d = bl602_hal::delay::McycleDelay::new(clocks.sysclk().0);
+    serial.write_str("Starting i2c test\r\n").ok();
 
+    for byte_pair in I2C_CMDS.iter() {
+        let result = i2c.try_write(0x20, byte_pair);
+        match result {
+            Ok(_) => {
+                serial.write_str("writing success\r\n").ok();
+            }
+            Err(e) => {
+                serial.write_str(i2c_err_strings(e)).ok();
+            }
+        }
+        d.try_delay_us(100).unwrap();
+    }
+
+    serial.write_str("Finished i2c test\r\n\r\n").ok();
     loop {
-        // Toggle the LED on and off once a second. Report LED status over UART
-        gpio5.try_set_high().unwrap();
-        serial.write_str("LEDs on\r\n").ok();
+        gpio5.try_toggle().unwrap();
         d.try_delay_ms(1000).unwrap();
+    }
+}
 
-        gpio5.try_set_low().unwrap();
-        serial.write_str("LEDs off\r\n").ok();
-        d.try_delay_ms(1000).unwrap();
+fn i2c_err_strings(e: hal::i2c::Error) -> &'static str {
+    match e {
+        hal::i2c::Error::RxOverflow => "err RxOverflow\r\n",
+        hal::i2c::Error::RxUnderflow => "err RxUnderflow\r\n",
+        hal::i2c::Error::TxOverflow => "err TxOverflow\r\n",
+        hal::i2c::Error::TxUnderflow => "err TxUnderflow\r\n",
+        hal::i2c::Error::Timeout => "err Timeout\r\n",
     }
 }
